54167d4a90ca253d5264b447d797c45280045675
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-02-27 19:19:02 +0000

    tailwind and main layout

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,5 +8,10 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+  use AuthorizesRequests, ValidatesRequests;
+
+  public function __construct()
+  {
+
+  }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('title', 'Home')
+@section('content')
+  <h1>Home</h1>
+  <p>Welcome to our home page</p>
+@endsection
