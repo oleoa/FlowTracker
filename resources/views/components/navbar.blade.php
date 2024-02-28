ac6d9f8dd56f96acddc822fd891e8eaa5a44ac94
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-02-28 16:18:23 +0000

    navbar responsive

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -24,9 +24,36 @@
 
 </nav>
 
-<nav class="xl:hidden flex fixed bottom-0 left-0 w-full h-28 z-20 p-4 bg-secondary-100">
+<nav class="xl:hidden flex fixed bottom-0 left-0 w-full h-24 z-20 p-4 bg-secondary-100">
 
   <div class="bg-secondary-200 w-full h-full rounded">
+    
+    <div class="w-full h-full flex justify-start">
+      <a href="{{route('dashboard')}}" @class([
+          'p-4 w-full flex justify-center items-center rounded',
+          'bg-gradient-to-br from-primary-100 to-primary-300' => $current == 'dashboard'
+        ])>
+        <i class="fa-solid fa-chart-line"></i>
+      </a>
+      <a href="{{route('expenses')}}" @class([
+        'p-4 w-full flex justify-center items-center rounded',
+        'bg-gradient-to-br from-primary-100 to-primary-300' => $current == 'expenses'
+        ])>
+        <i class="fa-solid fa-money-check-dollar"></i>
+      </a>
+      <a href="{{route('incomes')}}" @class([
+          'p-4 w-full flex justify-center items-center rounded',
+          'bg-gradient-to-br from-primary-100 to-primary-300' => $current == 'incomes'
+        ])>
+        <i class="fa-solid fa-sack-dollar"></i>
+      </a>
+      <a href="{{route('dashboard')}}" @class([
+          'p-4 w-full flex justify-center items-center rounded',
+          'bg-gradient-to-br from-primary-100 to-primary-300' => $current == 'settings'
+        ])>
+        <i class="fa-solid fa-gear"></i>
+      </a>
+    </div>
 
   </div>
 
